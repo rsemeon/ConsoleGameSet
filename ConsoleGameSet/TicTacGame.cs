36251ddd6948ca74b21f5f719ffcd7613cfa7537
Richard Semeon <rsemeon@gmail.com>
2020-07-07 18:05:41 +1000

    initial commit for TicTacToe game

---
+++ ConsoleGameSet/TicTacGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGameSet
+{
+    class TicTacGame : ConsoleGame
+    {
+        enum PlayOptions
+        {
+            O,
+            X
+        }
+
+        TicTacBoard board = new TicTacBoard();
+        PlayOptions currentTurn = PlayOptions.X; // X always starts
+        TicTacPlayer player = new TicTacUser();
+        String winner;
+
+        public TicTacGame(string name, string description) : base(name, description)
+        {
+        }
+
+        public override void ResetGame()
+        {
+
+        }
+
+        public override void NextMove()
+        {
+
+        }
+
+        public override void Draw()
+        {
+            board.DrawBoard();
+        }
+
+        public override bool IsGameOver()
+        {
+            if (GetWinner() != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string GetWinner()
+        {
+            if (String.IsNullOrWhiteSpace(winner))
+            {
+                winner = board.CheckStatus();
+            }
+
+            return winner;
+        }
+    }
+}
---
+++ ConsoleGameSet/TicTacPlayer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGameSet
+{
+    class TicTacPlayer : ConsolePlayer
+    {
+        public override int[] GetMove(TicTacBoard board)
+        {
+            bool validInput;
+            string[] userInputSplit;
+            string[] userChoiceString;
+            int[] move = { 0, 0 };
+
+            do
+            {
+                validInput = true;
+
+                Console.Write("\n  Enter a grid position (e.g. x,y ) : ");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "quit" || userInput == "q" || userInput == "exit")
+                {
+                    Environment.Exit(0);
+                }
+
+                if (!String.IsNullOrWhiteSpace(userInput))
+                {
+                    if (userInput.Contains(","))
+                    {
+                        userChoiceString = userInput.Split(",");
+                    }
+                    else
+                    {
+                        userInputSplit = Regex.Split(userInput, "");
+                        userChoiceString = new string[] { userInputSplit[1], userInputSplit[2] };
+                    }
+
+                    if (userChoiceString.Length != 2)
+                    {
+                        validInput = false;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            move[0] = int.Parse(userChoiceString[0]);
+                            move[1] = int.Parse(userChoiceString[1]);
+                        }
+                        catch
+                        {
+                            validInput = false;
+                        }
+                    }
+
+                    if (move[0] < 1 || move[0] > board.BoardSize) { validInput = false; }
+                    if (move[1] < 1 || move[1] > board.BoardSize) { validInput = false; }
+
+                    move[0]--;
+                    move[1]--;
+
+                }
+
+                if (validInput)
+                {
+                    return move;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input try again!");
+                }
+
+            } while (!validInput);
+
+            return move;
+        }
+    }
+}
