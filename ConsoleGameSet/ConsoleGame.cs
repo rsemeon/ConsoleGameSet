01201b3dc12c820bc7764582f5b2883bfbffe8cf
Richard Semeon <rsemeon@gmail.com>
2020-07-07 15:18:31 +1000

    abstract classes for Games and Player

---
+++ ConsoleGameSet/ConsoleGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGameSet
+{
+    abstract class ConsoleGame
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ConsoleGame(string name, string description) {
+            Name = name;
+            Description = description;
+        }
+
+        public abstract void ResetGame();
+
+        public abstract void GetNewMove();
+
+        public abstract void DrawBoard();
+
+        public abstract void IsGameOver();
+
+    }
+}
---
+++ ConsoleGameSet/ConsolePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGameSet
+{
+    abstract class ConsolePlayer
+    {
+        public string tag;
+
+        public override string ToString()
+        {
+            return tag;
+        }
+
+        abstract public int[] GetMove(ConsoleBoard board);
+
+
+    }
+}
